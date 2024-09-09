80649d72334a20c084ad6c5e1207d2bc5262c91a
Hyun Dong Lee <hyundonglee1015@gmail.com>
2024-09-09 15:43:15 +0900

    ukf sampler

--- dynamax/linear_gaussian_ssm/models.py
+++ dynamax/linear_gaussian_ssm/models.py
@@ -1179,7 +1179,7 @@ class GrassmannianGaussianConjugateSSM(LinearGaussianSSM):
             sample_size: int,
             emissions: Float[Array, "nbatch ntime emission_dim"],
             base_subspace,
-            ieks_num_iter = 100,
+            # ieks_num_iter = 100,
             inputs: Optional[Float[Array, "nbatch ntime input_dim"]] = None,
             return_states: bool = False,
             return_n_samples: int = 100,
