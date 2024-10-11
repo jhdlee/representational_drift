cross covariance bug

dynamax/linear_gaussian_ssm/models.py
@@ -1964,10 +1964,10 @@ class GrassmannianGaussianConjugateSSM(LinearGaussianSSM):
                 S, m = _params.initial.cov, _params.initial.mean
             else:
                 init_stats_1, init_stats_2 = init_stats
-                VxS = init_stats_2 + jnp.einsum('b,cij->bcij', jnp.ones(num_trials), _params.initial.cov)
-                VxSinv = jnp.einsum('bc,bcij->bcij', conditions_one_hot, jnp.linalg.inv(VxS))
-                initial_mean_stats_1 = VxSinv.sum(0)
-                initial_mean_stats_2 = jnp.einsum('bcij,bcj->ci', VxSinv, init_stats_1)
+                Sinv = jnp.linalg.inv(_params.initial.cov)
+                cSinv = jnp.einsum('bc,cij->bcij', conditions_one_hot, Sinv)
+                initial_mean_stats_1 = cSinv.sum(0)
+                initial_mean_stats_2 = jnp.einsum('bci,bcij->cj', init_stats_1, cSinv)
                 initial_mean_stats = (initial_mean_stats_1, initial_mean_stats_2)
                 initial_mean_posterior = mvn_posterior_update(self.initial_prior, initial_mean_stats)
                 m = initial_mean_posterior.mode()
@@ -1990,7 +1990,6 @@ class GrassmannianGaussianConjugateSSM(LinearGaussianSSM):
 
                     S_c = vmap(_update_initial_cov)(initial_cov_stats_c_2)
                     return jnp.diag(S_c)
-
                 S = vmap(update_initial_cov)(initial_cov_stats_1, initial_cov_stats_2)
 
             # Update the dynamics params
@@ -2072,11 +2071,12 @@ class GrassmannianGaussianConjugateSSM(LinearGaussianSSM):
                 Ev = velocity_smoother.smoothed_means
                 H = vmap(rotate_subspace, in_axes=(None, None, 0))(base_subspace, self.state_dim, Ev)
 
-                VvS = velocity_smoother.smoothed_covariances[0] + _params.initial_velocity.cov
+                # VvS = velocity_smoother.smoothed_covariances[0] + _params.initial_velocity.cov
+                VvS = _params.initial_velocity.cov
                 VvSinv = jnp.linalg.inv(VvS)
                 Ev0 = velocity_smoother.smoothed_means[0]
                 initial_velocity_mean_stats_1 = VvSinv
-                initial_velocity_mean_stats_2 = jnp.einsum('ij,j->i', VvSinv, Ev0)
+                initial_velocity_mean_stats_2 = jnp.einsum('i,ij->j', Ev0, VvSinv)
                 initial_velocity_mean_stats = (initial_velocity_mean_stats_1, initial_velocity_mean_stats_2)
                 initial_velocity_mean_posterior = mvn_posterior_update(self.initial_velocity_prior, initial_velocity_mean_stats)
                 initial_velocity_mean = initial_velocity_mean_posterior.mode()

dynamax/nonlinear_gaussian_ssm/inference_ekf.py
@@ -350,12 +350,13 @@ def extended_kalman_smoother(
         smoothed_cov = filtered_cov + G @ (smoothed_cov_next - S_pred) @ G.T
 
         # Compute the smoothed expectation of z_t z_{t+1}^T
-        smoothed_cross = G @ smoothed_cov_next + jnp.outer(smoothed_mean, smoothed_mean_next)
+        smoothed_cross_cov = G @ smoothed_cov_next
+        smoothed_cross_outer = jnp.outer(smoothed_mean, smoothed_mean_next)
 
-        return (smoothed_mean, smoothed_cov), (smoothed_mean, smoothed_cov, smoothed_cross)
+        return (smoothed_mean, smoothed_cov), (smoothed_mean, smoothed_cov, smoothed_cross_cov, smoothed_cross_outer)
 
     # Run the extended Kalman smoother
-    _, (smoothed_means, smoothed_covs, smoothed_cross) = lax.scan(
+    _, (smoothed_means, smoothed_covs, smoothed_cross_cov, smoothed_cross_outer) = lax.scan(
         _step,
         (filtered_means[-1], filtered_covs[-1]),
         (jnp.arange(num_trials - 1), filtered_means[:-1], filtered_covs[:-1]),
@@ -365,7 +366,9 @@ def extended_kalman_smoother(
     # Concatenate the arrays and return
     smoothed_means = jnp.vstack((smoothed_means, filtered_means[-1][None, ...]))
     smoothed_covs = jnp.vstack((smoothed_covs, filtered_covs[-1][None, ...]))
-    smoothed_cross = smoothed_cross[::-1]
+    smoothed_cross_cov = smoothed_cross_cov[::-1]
+    smoothed_cross_outer = smoothed_cross_outer[::-1]
+    smoothed_cross = smoothed_cross_cov + smoothed_cross_outer
     return PosteriorGSSMSmoothed(
         marginal_loglik=ll,
         filtered_means=filtered_means,