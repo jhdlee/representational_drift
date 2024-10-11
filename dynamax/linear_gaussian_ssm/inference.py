cross covariance bug

dynamax/linear_gaussian_ssm/inference.py
@@ -607,9 +607,10 @@ def lgssm_smoother(
         smoothed_cov = filtered_cov + mask * G @ (smoothed_cov_next - F @ filtered_cov @ F.T - Q) @ G.T
 
         # Compute the smoothed expectation of z_t z_{t+1}^T
-        smoothed_cross = G @ smoothed_cov_next + jnp.outer(smoothed_mean, smoothed_mean_next)
+        smoothed_cross_cov = G @ smoothed_cov_next
+        smoothed_cross_outer = jnp.outer(smoothed_mean, smoothed_mean_next)
 
-        return (smoothed_mean, smoothed_cov), (smoothed_mean, smoothed_cov, smoothed_cross)
+        return (smoothed_mean, smoothed_cov), (smoothed_mean, smoothed_cov, smoothed_cross_cov, smoothed_cross_outer)
 
     # Run the Kalman smoother
     init_carry = (filtered_means[-1], filtered_covs[-1])
@@ -617,12 +618,14 @@ def lgssm_smoother(
             filtered_means[:-1][::-1],
             filtered_covs[:-1][::-1],
             masks[1:][::-1])
-    _, (smoothed_means, smoothed_covs, smoothed_cross) = lax.scan(_step, init_carry, args)
+    _, (smoothed_means, smoothed_covs, smoothed_cross_cov, smoothed_cross_outer) = lax.scan(_step, init_carry, args)
 
     # Reverse the arrays and return
     smoothed_means = jnp.vstack((smoothed_means[::-1], filtered_means[-1][None, ...]))
     smoothed_covs = jnp.vstack((smoothed_covs[::-1], filtered_covs[-1][None, ...]))
-    smoothed_cross = smoothed_cross[::-1]
+    smoothed_cross_cov = smoothed_cross_cov[::-1]
+    smoothed_cross_outer = smoothed_cross_outer[::-1]
+    smoothed_cross = smoothed_cross_cov + smoothed_cross_outer
     return PosteriorGSSMSmoothed(
         marginal_loglik=ll,
         filtered_means=filtered_means,
@@ -631,7 +634,6 @@ def lgssm_smoother(
         smoothed_covariances=smoothed_covs,
         smoothed_cross_covariances=smoothed_cross,
     )
-
 
 @preprocess_args_for_sampler
 def lgssm_posterior_sample(