dfdc296e8f8b9ae3ef27078187e35d95865ed87e
Hyun Dong Lee <hyundonglee1015@gmail.com>
2025-03-29 23:43:20 -0400

    normalize pred cov

--- dynamax/nonlinear_gaussian_ssm/inference_ekf.py
+++ dynamax/nonlinear_gaussian_ssm/inference_ekf.py
@@ -1036,18 +1036,18 @@ def extended_kalman_filter(
             inputs = (_pred_mean, _pred_cov)
             filtered_mean, filtered_cov = jax.lax.cond(trial_mask, true_fun, false_fun, inputs)
 
+            # Predict the next state
+            pred_mean, pred_cov = _predict(filtered_mean, filtered_cov, Q)
+
             # normalize the eigenvalues of the predicted covariance by their maximum
             # L, U = jnp.linalg.eigh(filtered_cov)
-            lambda_max, _ = power_iteration(filtered_cov)
+            lambda_max, _ = power_iteration(pred_cov)
             threshold = 1e-3
             should_normalize = lambda_max > threshold
             # jax.debug.print('max_eigval: {max_eigval}', max_eigval=jnp.max(L))
-            filtered_cov = jnp.where(should_normalize, 
-                                     threshold * filtered_cov / lambda_max, 
-                                     filtered_cov)
-
-            # Predict the next state
-            pred_mean, pred_cov = _predict(filtered_mean, filtered_cov, Q)
+            pred_cov = jnp.where(should_normalize, 
+                                 threshold * pred_cov / lambda_max, 
+                                 pred_cov)
 
         # Build carry and output states
         carry = (ll, pred_mean, pred_cov)
