6201f3f0779925439dd8627992d9aa1c5e6b060f
Hyun Dong Lee <hyundonglee1015@gmail.com>
2025-04-11 16:48:00 -0700

    simulated sine wave smds

--- scripts/train_smds_simulated.py
+++ scripts/train_smds_simulated.py
@@ -134,7 +134,7 @@ def main(config: DictConfig):
         if data_config.velocity_type == 'sine':
             _velocity = jnp.zeros((num_trials,) + dof_shape)
             # sine_wave = 0.5*jnp.sin(jnp.linspace(-jnp.pi, jnp.pi, num_trials))
-            sine_wave = 0.5*jnp.sin(jnp.linspace(-2*jnp.pi, 2*jnp.pi, num_trials))
+            sine_wave = 0.5*jnp.pi*jnp.sin(jnp.linspace(-jnp.pi, jnp.pi, num_trials))
             _velocity = _velocity.at[:, 0, 0].set(sine_wave)
             # set true tau to the MLE of the sine wave
             true_tau = jnp.ones(dof) * 1e-32
