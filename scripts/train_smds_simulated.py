b4f05556993842e27bb127347bf46597b69b529a
Hyun Dong Lee <hyundonglee1015@gmail.com>
2025-04-11 13:58:00 -0700

    simulated sine wave smds

--- scripts/train_smds_simulated.py
+++ scripts/train_smds_simulated.py
@@ -79,11 +79,11 @@ def main(config: DictConfig):
     dof = true_state_dim * (emission_dim - true_state_dim)
     dof_shape = (true_state_dim, emission_dim - true_state_dim)
 
-    if (model_config.type == 'smds' 
-        and model_config.state_dim > training_config.smds_test_state_dim_bound):
-        print(f"Skipping {model_config.state_dim} because state dim is too large")
-        # Skip the rest of the function
-        return
+    # if (model_config.type == 'smds' 
+    #     and model_config.state_dim > training_config.smds_test_state_dim_bound):
+    #     print(f"Skipping {model_config.state_dim} because state dim is too large")
+    #     # Skip the rest of the function
+    #     return
 
     data_dir = model_dir = f'/oak/stanford/groups/swl1/hdlee/smds/simulated_{true_state_dim}x{emission_dim}/'
     os.makedirs(data_dir, exist_ok=True)
